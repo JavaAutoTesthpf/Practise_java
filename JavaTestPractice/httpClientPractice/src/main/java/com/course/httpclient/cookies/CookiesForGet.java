7ca33c66697b25225358ad4a51d421ff024797bb
felixhanpf <dx930206@gmail.com>
2021-09-07 09:34:36 +0800

    最新代码提交20210907

---
+++ JavaTestPractice/httpClientPractice/src/main/java/com/course/httpclient/cookies/CookiesForGet.java
@@ -0,0 +1,58 @@
+package com.course.httpclient.cookies;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class CookiesForGet {
+    private String url;
+    private ResourceBundle bundle;
+
+    @BeforeTest
+    public void beforeTest() {
+        bundle = ResourceBundle.getBundle("application");
+        url = bundle.getString("test.url");
+    }
+
+    @Test(invocationCount=2)
+    public void testGetCookies() throws IOException {
+        String result;
+        //从配置文件中拼接测试的url
+        String uri = bundle.getString("getCookies.uri");
+        String testUrl = url + uri;
+
+        //测试代码逻辑书写
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        BasicCookieStore cookieStore = new BasicCookieStore();
+        HttpGet get = new HttpGet(testUrl);
+        //必须是声明为DefaultHttpClient才能获取cookie，HttpClient不可以获取cookie
+        httpclient = HttpClients.custom().setDefaultCookieStore(cookieStore).build();
+        HttpResponse response = httpclient.execute(get);
+        result = EntityUtils.toString(response.getEntity(), "UTF-8");
+        System.out.println(result);
+
+        //H获取cookie信息
+        List<Cookie> cookieList = cookieStore.getCookies();
+        for (Cookie cookie : cookieList) {
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie name = "+name+" \ncookie value = "+value);
+        }
+
+    }
+
+}
---
+++ JavaTestPractice/httpClientPractice/src/main/java/com/course/httpclient/demo/HttpDemo.java
@@ -0,0 +1,23 @@
+package com.course.httpclient.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class HttpDemo {
+
+    @Test
+    public void test1() throws IOException {
+        String result;//用来存放结果
+        HttpGet get = new HttpGet("http://www.baidu.com");
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(), "utf-8");
+        System.out.println(result);
+    }
+}
